f65a6d3dade14227f4e9f5c5a0bbcfce077ae0d3
liuying19403259 <ly794613@sina.com>
2020-03-30 18:04:08 +0800

    milestone3

--- assignment2.py
+++ assignment2.py
@@ -4,6 +4,7 @@ import os
 import sys
 import redis
 import json
+import requests
 
 from argparse import ArgumentParser
 
@@ -16,11 +17,12 @@ from linebot.exceptions import (
 )
 
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageMessage, VideoMessage, VideoSendMessage, FileMessage, StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage, TemplateSendMessage, ImageCarouselTemplate, ImageCarouselColumn, PostbackTemplateAction
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage, VideoMessage, VideoSendMessage, FileMessage, StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage, TemplateSendMessage, ImageCarouselTemplate, ImageCarouselColumn, PostbackTemplateAction, CarouselTemplate, CarouselColumn, URITemplateAction
 )
 from linebot.utils import PY3
 
 from string import Template
+
 
 HOST ="redis-11707.c73.us-east-1-2.ec2.cloud.redislabs.com"
 PWD = "fzUPMVnd6cvDmBYjPuioH2FHP8n5oGvR"
@@ -28,28 +30,96 @@ PORT = "11707"
 redis1 = redis.Redis(host = HOST, password = PWD, port = PORT)
 
 
-redis1.set("COVID","COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019. Until 12th Mar, there are totally 118325 confirmed case globally")
-test=redis1.get("COVID").decode('UTF-8')
+redis1.set("COVID-19 Introduction","COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019. Until 12th Mar, there are totally 118325 confirmed case globally")
+test=redis1.get("COVID-19 Introduction").decode('UTF-8')
 
-redis1.set("incubation period","COVID-19 ranges from 1-14 days, most commonly around five days.")
-test1=redis1.get("incubation period").decode('UTF-8')
+redis1.set("Incubation Period","COVID-19 ranges from 1-14 days, most commonly around five days.")
+test1=redis1.get("Incubation Period").decode('UTF-8')
 
-redis1.lpush("COVID-19","COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019. Until 12th Mar, there are totally 118325 confirmed case globally","COVID-19 ranges from 1-14 days, most commonly around five days.","https://i0.wp.com/www.newtelegraphng.com/wp-content/uploads/2020/01/Coronavirus.png?w=622&ssl=1","https://cdn.the-scientist.com/assets/articleNo/67045/aImg/35755/coronavirus-incubation-l.jpg")
+redis1.lpush("COVID-19","COVID-19 is the infectious disease caused by the most recently discovered coronavirus. This new virus and disease were unknown before the outbreak began in Wuhan, China, in December 2019. Until 12th Mar, there are totally 118325 confirmed case globally",
+             "COVID-19 ranges from 1-14 days, most commonly around five days.",
+             "https://i0.wp.com/www.newtelegraphng.com/wp-content/uploads/2020/01/Coronavirus.png?w=622&ssl=1",
+             "https://cdn.the-scientist.com/assets/articleNo/67045/aImg/35755/coronavirus-incubation-l.jpg")
 test11=redis1.lindex("COVID-19",3).decode('UTF-8')
 test10=redis1.lindex("COVID-19",2).decode('UTF-8')
 test2=redis1.lindex("COVID-19",1)
 test3=redis1.lindex("COVID-19",0)
 
 
-redis1.lpush("mask","龍城大藥房","香港特別行政區油尖旺區尖沙咀加連威老道28號","22.302755","114.185589")
-test4=redis1.lindex("mask",3).decode('UTF-8')
-test5=redis1.lindex("mask",2).decode('UTF-8')
-test6=redis1.lindex("mask",1)
-test7=redis1.lindex("mask",0)
+redis1.lpush("1","龍城大藥房","尖沙咀加連威老道28號","22.302755","114.185589")
+test4=redis1.lindex("1",3).decode('UTF-8')
+test5=redis1.lindex("1",2).decode('UTF-8')
+test6=redis1.lindex("1",1)
+test7=redis1.lindex("1",0)
 
-redis1.lpush("symptoms","https://www.youtube.com/watch?v=fg3SArRmYOI","https://cdn.images.express.co.uk/img/dynamic/151/590x/coronavirus-uk-face-mask-safety-development-world-first-covid-19-resistant-mask-1254044.jpg?r=1584025841438")
+
+redis1.lpush("2","華潤堂","銅鑼灣軒尼詩道488-490號軒尼詩大廈地下C鋪","22.28271","114.19439")
+test32=redis1.lindex("2",3).decode('UTF-8')
+test33=redis1.lindex("2",2).decode('UTF-8')
+test34=redis1.lindex("2",1)
+test35=redis1.lindex("2",0)
+
+
+redis1.lpush("3","仁豐藥房","Shatin Plaza, L3/F, 41, 42A","22.3820646","114.1888745")
+test36=redis1.lindex("3",3).decode('UTF-8')
+test37=redis1.lindex("3",2).decode('UTF-8')
+test38=redis1.lindex("3",1)
+test39=redis1.lindex("3",0)
+
+
+redis1.lpush("4","志成大藥房","Tsuen Wan, Ham Tin St, 46號地下 Shing On Building","22.3675929","114.112213")
+test40=redis1.lindex("4",3).decode('UTF-8')
+test41=redis1.lindex("4",2).decode('UTF-8')
+test42=redis1.lindex("4",1)
+test43=redis1.lindex("4",0)
+
+
+redis1.lpush("5","龍豐药房","Yuen Long, 壽富街 19號 地下 龍 豐藥房","22.4445157","114.0232547")
+test44=redis1.lindex("5",3).decode('UTF-8')
+test45=redis1.lindex("5",2).decode('UTF-8')
+test46=redis1.lindex("5",1)
+test47=redis1.lindex("5",0)
+
+
+redis1.lpush("symptoms","http://cloudvideo.thepaper.cn/sparker/f954b6f1c81d4c44a5e4d2a437a1fe2a/ld/9db4ba09-1427-4503-b1b3-6f68fc630ed8-659e9aa5-4ff1-1dd0-5f12-0df81cc3bf64.mp4",
+             "https://cdn.images.express.co.uk/img/dynamic/151/590x/coronavirus-uk-face-mask-safety-development-world-first-covid-19-resistant-mask-1254044.jpg?r=1584025841438")
 test8=redis1.lindex("symptoms",1)
 test9=redis1.lindex("symptoms",0)
+
+redis1.lpush("COVID News","發佈日期：2020-03-19","香港地區個案累計209宗，新增16宗","點擊查看詳細報道","https://www.info.gov.hk/gia/general/202003/18/P2020031800905.htm",
+                          "發佈日期：2020-03-18","香港地區個案累計193宗，新增25宗","點擊查看詳細報道","https://www.info.gov.hk/gia/general/202003/18/P2020031800905.htm",
+                          "發佈日期：2020-03-17","香港地區個案累計168宗，新增10宗","點擊查看詳細報道","https://www.info.gov.hk/gia/general/202003/18/P2020031700784.htm",
+                          "發佈日期：2020-03-16","香港地區個案累計158宗，新增9宗","點擊查看詳細報道","https://www.info.gov.hk/gia/general/202003/16/P2020031600810.htm",
+                          "發佈日期：2020-03-15","香港地區個案累計149宗，新增7宗","點擊查看詳細報道","https://www.info.gov.hk/gia/general/202003/15/P2020031500734.htm")
+test12=redis1.lindex("COVID News",19).decode('UTF-8')
+test13=redis1.lindex("COVID News",18).decode('UTF-8')
+test14=redis1.lindex("COVID News",17).decode('UTF-8')
+test15=redis1.lindex("COVID News",16)
+
+test16=redis1.lindex("COVID News",15).decode('UTF-8')
+test17=redis1.lindex("COVID News",14).decode('UTF-8')
+test18=redis1.lindex("COVID News",13).decode('UTF-8')
+test19=redis1.lindex("COVID News",12)
+
+test20=redis1.lindex("COVID News",11).decode('UTF-8')
+test21=redis1.lindex("COVID News",10).decode('UTF-8')
+test22=redis1.lindex("COVID News",9).decode('UTF-8')
+test23=redis1.lindex("COVID News",8)
+
+test24=redis1.lindex("COVID News",7).decode('UTF-8')
+test25=redis1.lindex("COVID News",6).decode('UTF-8')
+test26=redis1.lindex("COVID News",5).decode('UTF-8')
+test27=redis1.lindex("COVID News",4)
+
+test28=redis1.lindex("COVID News",3).decode('UTF-8')
+test29=redis1.lindex("COVID News",2).decode('UTF-8')
+test30=redis1.lindex("COVID News",1).decode('UTF-8')
+test31=redis1.lindex("COVID News",0)
+
+
+
+
+
 
 
 
@@ -113,16 +183,18 @@ def callback():
 
     return 'OK'
 
+
+
 # Handler function for Text Message
 def handle_TextMessage(event):
     print(event.message.text)
-    if(event.message.text=="COVID"):
+    if(event.message.text=="COVID-19 Introduction"):
         line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(test)
     )
 
-    elif(event.message.text=="incubation period"):
+    elif(event.message.text=="Incubation Period"):
         line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(test1)
@@ -137,7 +209,7 @@ def handle_TextMessage(event):
                         image_url= test2.decode(),
                         action=PostbackTemplateAction(
                         label='Introduction',
-                        text= test11,
+                        text= 'COVID-19 Introduction',
                         data='action=buy&itemid=1'
                         )
                     ),
@@ -145,7 +217,7 @@ def handle_TextMessage(event):
                         image_url= test3.decode(),
                         action=PostbackTemplateAction(
                         label='Incubation',
-                        text= test10,
+                        text= "Incubation Period",
                         data='action=buy&itemid=2'
                         )
                     )
@@ -156,7 +228,19 @@ def handle_TextMessage(event):
 
 
 
+
+
+
     elif(event.message.text=="mask"):
+        print(event.message.text)
+        msg = 'Which district do you live? (Please type number)\n1.油尖旺區\n2.灣仔區\n3.沙田區\n4.荃灣區\n5.觀塘區' 
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(msg)
+    )
+    
+    
+    elif(event.message.text=="1"):
         message = LocationSendMessage(
             title= test4,
             address= test5,
@@ -166,16 +250,133 @@ def handle_TextMessage(event):
         line_bot_api.reply_message(
         event.reply_token, 
         message)
+    
+
+    elif(event.message.text=="2"):
+        message = LocationSendMessage(
+            title= test32,
+            address= test33,
+            latitude= test34.decode(),
+            longitude= test35.decode()
+        )
+        line_bot_api.reply_message(
+        event.reply_token, 
+        message)
+    
+        
+        
+    elif(event.message.text=="3"):
+        message = LocationSendMessage(
+            title= test36,
+            address= test37,
+            latitude= test38.decode(),
+            longitude= test39.decode()
+        )
+        line_bot_api.reply_message(
+        event.reply_token, 
+        message)
+    
+    
+    elif(event.message.text=="4"):
+        message = LocationSendMessage(
+            title= test40,
+            address= test41,
+            latitude= test42.decode(),
+            longitude= test43.decode()
+        )
+        line_bot_api.reply_message(
+        event.reply_token, 
+        message)
+    
+    
+    elif(event.message.text=="5"):
+        message = LocationSendMessage(
+            title= test44,
+            address= test45,
+            latitude= test46.decode(),
+            longitude= test47.decode()
+        )
+        line_bot_api.reply_message(
+        event.reply_token, 
+        message)
 
 
 
-    elif(event.message.text=="symptoms"):
+    elif(event.message.text=="wash hands"):
         message = VideoSendMessage(
-            original_content_url= test8.decode(),
-            preview_image_url= test9.decode()
+            original_content_url = 'https://firebasestorage.googleapis.com/v0/b/linechatbot-groups.appspot.com/o/linevideo%2Fmimi%202020-03-03%2017.25.15.mp4?alt=media&token=d001bac0-847b-455a-ad7e-2408ee86f79a',
+            preview_image_url= 'https://firebasestorage.googleapis.com/v0/b/linechatbot-groups.appspot.com/o/linevideo%2FIMG_0869.PNG?alt=media&token=81a8b13c-3d16-4a26-ba11-8e4d0e2c5ad8'
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    
+    
+
+    elif(event.message.text=="COVID News"):
+        message = TemplateSendMessage(
+            alt_text='Carousel template',
+            template=CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        title= test12,
+                        text= test13,
+                        actions=[
+                            URITemplateAction(
+                               label= test14,
+                               uri= test15.decode()
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        title= test16,
+                        text= test17,
+                        actions=[
+                            URITemplateAction(
+                               label= test18,
+                               uri= test19.decode()
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        title= test20,
+                        text= test21,
+                        actions=[
+                            URITemplateAction(
+                               label= test22,
+                               uri= test23.decode()
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        title= test24,
+                        text= test25,
+                        actions=[
+                            URITemplateAction(
+                               label= test26,
+                               uri= test27.decode()
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        title= test28,
+                        text= test29,
+                        actions=[
+                            URITemplateAction(
+                               label= test30,
+                               uri= test31.decode()
+                            )
+                        ]
+                    ),
+                ]
+            )
         )
         line_bot_api.reply_message(event.reply_token, message)
 
+    else:
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage('請輸入正確的關鍵字：\n1.如果您想查找新冠肺炎的相關介紹，請輸入：COVID-19\n2.如果您想查詢哪裡可以買口罩，請輸入：mask\n3.如果您想知道如何正確洗手，請輸入：wash hands\n4.如果您想了解最新的新冠肺炎發展趨勢，請輸入：COVID News')
+        )
+        
     
         
 
--- spec.md
+++ spec.md
@@ -3,6 +3,6 @@ Group S:
 1.Version: LINE
 2.Introduction: This project is to design a Line chatbot to provide the information about the public health care. The chatbot works as the format like: when user send some key words about COVID-19, Incubation, mask, etc. to the chatbot, it will send back some information about the key words, such as how to prevent COVID.
 3.Design:
-(1) Zhuangxiaoxin(19439962)：input “wash hands", the chatbot will return a video about seven-step hand washing(from youtube or some other video website) ; input "COVID", the bot will return some contents about it.
-(2) LIU Ying(19403259): When user send message "COVID-19", the chatbot will send back pictures. When user click on the picture, the chatbot will send back some "COVID-19" related contents.
-(3) Wangfei(19423691): When the user enters "symptoms", the chatbot will return the symptoms related video. When the user send "mask", the chatbot will send back the address where you can buy mask.
+(1) Zhuangxiaoxin(19439962)：User inputs “wash hands", the chatbot will return a video about seven-step hand washing(use firebase as the server to store the video information) ; When user input "COVID News", the bot will send back the lastest news about the spreading of COVID-19.
+(2) LIU Ying(19403259): When user send message "COVID-19", the chatbot will send back pictures with keywords. When user click the picture, the chatbot will send back some "COVID-19" related contents.
+(3) Wangfei(19423691):  When the user send "mask", the chatbot will ask the user where do him live, and then send back the address to buy mask in the area.
